09c0d82f596c4264b12ea0feaff6c5faeb5c4979
tejas-sangale01 <tejassangale55@gmail.com>
2024-11-20 10:56:14 +0530

    Max points you can obtain from cards

---
+++ 2-pointers/Maximum Points You Can Obtain from Cards/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int sum = 0, maxSum;
+        for(int i = 0; i < k; i++) sum += cardPoints[i];
+        maxSum = sum;
+        for(int i = 0; i<k; i++) {
+            sum -= cardPoints[k-i-1];
+            sum += cardPoints[n-i-1];
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
+};
